38722005a7363cd3fc275459771237cb3c5fdeaa
Steve <Steve@SFR-2018-XPS-15>
2023-07-14 16:04:53 -0400

    Add project files.

---
+++ TableInputDemoNonBlazor/TableCell.cs
@@ -0,0 +1,16 @@
+﻿namespace TableInputDemoNonBlazor
+    {
+    public class TableCell
+        {
+        public string ColName { get; set; } = string.Empty;
+        public string CSSClass { get; set; } = string.Empty;
+        public int Index_Col { get; set; }
+        public int Index_Row { get; set; }
+        public decimal Value_Dec { get; set; }
+        public string Value_Formatted { get; set; } = string.Empty;
+
+        public TableCell()
+            {
+            }
+        }
+    }
